b234c2f5b664e4b2d4cf33a22890df0f9534f577
Samit Watve <72366011+samitwatve@users.noreply.github.com>
2023-02-14 22:50:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,25 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+# """
+# # Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+# forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# In the meantime, below is an example of what you can do with just a few lines of code:
+# """
 
-def main():
-    st.title("Global Entry Notifier")
-    st.write("Welcome to my first Streamlit app.")
+st.title("Global Entry Notifier")
+ 
 
-if __name__ == '__main__':
-    main()
+cities = ['New York', 'London', 'Paris', 'Tokyo']
+
+selected_cities = st.multiselect('Select cities', cities)
+
+st.write('You selected:', selected_cities)
 
 # with st.echo(code_location='below'):
 #     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
