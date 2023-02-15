12301edbccdcca8215b0041bf3f21a2b80e3f593
Samit Watve <72366011+samitwatve@users.noreply.github.com>
2023-02-14 22:44:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,12 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+def main():
+    st.title("Hello, World!")
+    st.write("Welcome to my first Streamlit app.")
+
+if __name__ == '__main__':
+    main()
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
