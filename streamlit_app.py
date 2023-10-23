80c17ea52c4f0903082c7c2dbc1bc602bb54af66
Samit Watve <72366011+samitwatve@users.noreply.github.com>
2023-10-23 12:17:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -283,7 +283,7 @@ for stock in selected_stocks:
            temp = pd.DataFrame.copy(combined_df)
            
            ## Process and filter the dataframe
-           processed_df = massage_dataframe(temp, target_price_multiplier = target_price_multiplier, cost_basis)
+           processed_df = massage_dataframe(temp, target_price_multiplier = target_price_multiplier, cost_basis = cost_basis)
            filtered_df = filter_dataframe(processed_df)
            st.text(f"Processed {len(processed_df)} calls, {len(filtered_df)} remain after filtering")
            
